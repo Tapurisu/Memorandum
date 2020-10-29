87d6bd355c5a8c7bc5e035f25489793c4b5f1f30
shigure <327432952@qq.com>
2020-10-29 11:21:31 +0800

    更新备忘录编辑界面

---
+++ app/src/main/java/com/example/memorandum/MemoAdictor.java
@@ -0,0 +1,14 @@
+package com.example.memorandum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoAdictor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_adictor);
+    }
+}

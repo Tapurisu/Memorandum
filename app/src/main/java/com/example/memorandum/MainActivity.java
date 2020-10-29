92cef6f761c28040a4dcbd80285a7a8cfd12a98a
shigure <327432952@qq.com>
2020-10-29 17:10:53 +0800

    实现主界面与备忘录编辑界面之间的跳转，且备忘录编辑界面能获取并显示所选的备忘录的信息

--- app/src/main/java/com/example/memorandum/MainActivity.java
+++ app/src/main/java/com/example/memorandum/MainActivity.java
@@ -1,40 +1,55 @@
 package com.example.memorandum;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
     private static final String TAG = "MainActivity";
-    private String[] list_data ={"one", "two"};
-    private SimpleAdapter listItemAdapter;
+    List<MemoItem> memolist = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         DBManager dbManager = new DBManager(MainActivity.this);
-        List<MemoItem> memolist = dbManager.listAll();
+//        for(int i=0;i<10; i++){
+//            MemoItem memo = new MemoItem();
+//            memo.setId(i);
+//            memo.setMemoName("Memo"+i);
+//            memo.setMemoContent("This is the content of memo " + i);
+//            SimpleDateFormat formatter= new SimpleDateFormat("yyyy/MM/dd HH:mm");
+//            memo.setLastModificationTime(formatter.format(new Date(System.currentTimeMillis())));
+//            dbManager.add(memo);
+//        }
+
+        memolist = dbManager.listAll();
         Log.i(TAG, "memolist.size() = " + memolist.size() + "\n");
-//        int i = 0;
-//        for(MemoItem item : memolist){
-//            list_data[i] = item.getMemoName();
-//            Log.i(TAG, "list_data["+ i + "] = " + item.getMemoName() + "\n");
-//            i++;
-//        }
-//        for(int i = 0; i<5; i++){
-//            list_data[i] = dbManager.findById(i).getMemoName();
-//            Log.i(TAG, "list_data["+ i + "] = " + list_data[i] + "\n");
-//        }
         MyAdapter adapter = new MyAdapter(MainActivity.this,memolist);
         setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
 
-//        ListAdapter adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,list_data);
-//        setListAdapter(adapter);
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //Toast.makeText(MainActivity.this, "打开备忘录:" + memolist.get((int)id).getMemoName(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,MemoAdictor.class);
+        intent.putExtra("id", memolist.get(position).getId());
+        intent.putExtra("memoName", memolist.get(position).getMemoName());
+        intent.putExtra("memoContent", memolist.get(position).getMemoContent());
+        intent.putExtra("lastModificationTime", memolist.get(position).getLastModificationTime());
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/memorandum/MemoAdictor.java
+++ app/src/main/java/com/example/memorandum/MemoAdictor.java
@@ -2,13 +2,39 @@ package com.example.memorandum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MemoAdictor extends AppCompatActivity {
-
+    private final String TAG = "MemoAdictor";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_adictor);
+
+        Intent choosedMemo = getIntent();
+
+        EditText memoNameEditText = findViewById(R.id.memoNameEditText), memoContentEditText = findViewById(R.id.memoContentEditText);
+        TextView lastModificationTime = findViewById(R.id.LMTTextView);
+        memoNameEditText.setText(choosedMemo.getStringExtra("memoName"));
+        memoContentEditText.setText(choosedMemo.getStringExtra("memoContent"));
+        lastModificationTime.setText(choosedMemo.getStringExtra("lastModificationTime"));
+    }
+
+    public void starMemo(View view) {
+
+    }
+
+    public void deleteMemo(View view) {
+
+    }
+
+    public void saveMemo(View view) {
+
     }
 }
